208d7fae39096ec599e940faee9c9fc31f450085
Tom Palmer <remlapmot@hotmail.com>
2025-08-06 09:01:56 +0100

    Add helpfile for dataset

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' Example dataset
+#'
+#' @format ## `who`
+#' A data frame with 1 row and 3 columns:
+#' \describe{
+#'   \item{a}{"URL"}
+#'   \item{b}{"locuscomparer"}
+#'   \item{c}{"12345678"}
+#' }
+"config"
